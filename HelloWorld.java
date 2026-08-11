d7730c48410f93e57f1504530d8fb7f551642ffa
Chirag-Suvarna-builds <chirag.suvarna@ecolab.com>
2026-08-11 15:27:32 +0530

    Merge

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
    public static void main(String[] args) {
        System.out.println("Hello, World!");
 
-       System.out.println("Raising a pull request");
+       System.out.println("Merge Confict");
    }
 }
