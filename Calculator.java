12ed3c3939b5061718a3c59ccc9a39f0dda7f5af
Chirag-Suvarna-builds <chirag.suvarna@ecolab.com>
2026-08-11 16:15:29 +0530

    added logging

---
+++ Calculator.java
@@ -0,0 +1,12 @@
+public class Calculator {
+
+    public int add(int a, int b) {
+        System.out.println("Performing Addition");
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        System.out.println(calc.add(10, 20));
+    }
+}
